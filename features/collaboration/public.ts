8a61def03ee68fe3ae1623e6b490e710a91c9984
washwalk <patrickhadow@gmail.com>
2026-04-06 22:22:49 +0100

    fix: add missing feature directories for compliance

--- features/collaboration/public.ts
+++ features/collaboration/public.ts
@@ -7,7 +7,7 @@ export {
   revokeDraftInvite,
   removeSubmissionCollaborator,
   removeDraftCollaborator,
-} from '@/features/collaboration/collaboration-api'
-export { useDraftCollaborators } from '@/features/collaboration/use-draft-collaborators'
-export { useInviteLinkCopy } from '@/features/collaboration/use-invite-link-copy'
-export { useSubmissionCollaborators } from '@/features/collaboration/use-submission-collaborators'
+} from '@/features/collaboration/lib/collaboration-api'
+export { useDraftCollaborators } from '@/features/collaboration/hooks/use-draft-collaborators'
+export { useInviteLinkCopy } from '@/features/collaboration/hooks/use-invite-link-copy'
+export { useSubmissionCollaborators } from '@/features/collaboration/hooks/use-submission-collaborators'
