5c1689aebffa0ff00985ddbe0cccb2662760237e
washwalk <patrickhadow@gmail.com>
2026-04-16 19:19:34 +0100

    feat(social): add missing feature directories for compliance

---
+++ features/social/types/index.ts
@@ -0,0 +1,7 @@
+export type {
+  InstagramPostLayout,
+  InstagramPostRenderInput,
+} from '@/features/social/server/instagram-template'
+
+export const INSTAGRAM_POST_WIDTH = 1080
+export const INSTAGRAM_POST_HEIGHT = 1350
