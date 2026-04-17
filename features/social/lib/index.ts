87897c44ecc6ccb4dca598e0a2f9304b5db3b662
washwalk <patrickhadow@gmail.com>
2026-04-17 12:04:39 +0100

    fix: remove duplicate exports from social lib index

--- features/social/lib/index.ts
+++ features/social/lib/index.ts
@@ -1,7 +1,5 @@
 import { INSTAGRAM_POST_WIDTH, INSTAGRAM_POST_HEIGHT } from '@/features/social/types/index'
 
-export { INSTAGRAM_POST_WIDTH, INSTAGRAM_POST_HEIGHT }
-
 export interface SocialPlatformConfig {
   name: string
   dimensions: { width: number; height: number }
