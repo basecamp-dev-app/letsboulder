bec43d2f6128f94d53a92e67da0388adfe803cd2
washwalk <patrickhadow@gmail.com>
2026-04-22 09:24:34 +0100

    chore: remove dead barrels and bundle analyzer

--- features/admin/types/index.ts
+++
@@ -1,4 +0,0 @@
-export type { GymDiscipline, GymListItem, FloorPlan, EditableRoute } from '@/features/admin/gyms/types'
-export type { AdminCrag, CragImageRouteCandidate, MoveImageState } from '@/features/admin/crags/types'
-export { DISCIPLINE_OPTIONS, formatDiscipline } from '@/features/admin/gyms/types'
-export { formatRouteTypeLabel } from '@/features/admin/crags/types'
