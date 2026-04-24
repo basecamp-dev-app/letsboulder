053cdc40ffe2722a8ce8efa5bff22cf48e80b9fd
washwalk <patrickhadow@gmail.com>
2026-04-24 22:33:07 +0100

    fix: make public logbooks indexable

--- app/logbook/[userId]/layout.tsx
+++ app/logbook/[userId]/layout.tsx
@@ -1,13 +1,3 @@
-import { Metadata } from 'next'
-
-export const metadata: Metadata = {
-  title: 'Loading Logbook...',
-  robots: {
-    index: false,
-    follow: true,
-  },
-}
-
 export default function LogbookUserLayout({
   children,
 }: {
